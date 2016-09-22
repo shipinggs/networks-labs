6a550e63a8bc62d6a1acc0ac53910e3a0003673f
Shi Ping <limshiping93@hotmail.com>
2016-09-22 23:40:58 +0800

    Third: wrote pig's description

--- lab2/app.py
+++ lab2/app.py
@@ -22,8 +22,6 @@ def second():
         if form.validate() == False:
             flash('All fields are required.')
             return render_template('second.html', form = form)
-        else:
-            return render_template('success.html')
     elif request.method == 'GET':
          return render_template('second.html', form = form)
 
